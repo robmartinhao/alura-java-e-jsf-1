1be35bc3d2e50660f00881fdadd6f57cc934c06e
Robson Martinhão <robmartinhao@gmail.com>
2019-12-06 15:55:36 -0300

    Aula 04 - Completando o sistema e lidando com escopos do JSF 2.

--- src/main/java/br/com/alura/livraria/bean/LivroBean.java
+++ src/main/java/br/com/alura/livraria/bean/LivroBean.java
@@ -1,27 +1,52 @@
 package br.com.alura.livraria.bean;
 
 import br.com.alura.livraria.dao.DAO;
+import br.com.alura.livraria.modelo.Autor;
 import br.com.alura.livraria.modelo.Livro;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.List;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
-	private Livro livro = new Livro();
+    private Livro livro = new Livro();
+    private Integer autorId;
 
-	public Livro getLivro() {
-		return livro;
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public List<Autor> getAutores() {
+        return new DAO<Autor>(Autor.class).listaTodos();
+    }
+    public List<Autor> getAutoresDoLivro() {
+    	return this.livro.getAutores();
 	}
 
-	public void gravar() {
-		System.out.println("Gravando livro " + this.livro.getTitulo());
+    public void gravarAutor() {
+        Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+        this.livro.adicionaAutor(autor);
+        System.out.println("Livro escrito por: " + autor.getNome());
+    }
 
-		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
-		}
+    public void gravar() {
+        System.out.println("Gravando livro " + this.livro.getTitulo());
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
-	}
+        if (livro.getAutores().isEmpty()) {
+            throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+        }
 
+        new DAO<Livro>(Livro.class).adiciona(this.livro);
+    }
+
+    public Integer getAutorId() {
+        return autorId;
+    }
+
+    public void setAutorId(Integer autorId) {
+        this.autorId = autorId;
+    }
 }
